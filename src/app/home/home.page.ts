5a4985aa3ffc657cc84c85b9ef1212208dca29d5
gitErmesonAlves <ermesonalves2112@gmail.com>
2021-10-25 12:08:04 -0300

    fist commit to app

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+
+}
+
+
+export class MenuExample {
+
+constructor() { }
+
+}
